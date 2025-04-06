b1da35a719f3109000ee6fe8f23628dc1ff517ed
mort009900 <mnm86940@gmail.com>
2025-04-06 18:42:44 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,8 @@
 import os
 
-TOKEN = "7784061554:AAElO8fFvwPnJ9MPHXznfu2Qf8sokSjyqiQ"  # تأكد من صحة التوكن
-DB_PATH = "data/database.db"
+TOKEN = os.getenv ("7784061554:AAElO8fFvwPnJ9MPHXznfu2Qf8sokSjyqiQ")  # تأكد من صحة التوكن
+DB_PATH = os.getenv("data/database.db")
 BOOKS_FOLDER = "data/books/"
 PAGES_FOLDER = "data/pages/"
-PROXY_URL = None  # إذا كنت تحتاج إلى بروكسي، ضع الرابط هنا
-
+PROXY_URL = os.getenv(None)
 PAGES_SUBFOLDERS = [os.path.join(PAGES_FOLDER, f"FOLDER{i}") for i in range(1, 11)]
